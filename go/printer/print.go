193a3c5cb9ffef99957e7c10c8478bf6fd360138
Chris Boette <cboette@gmail.com>
2021-09-11 15:10:31 -0500

    Clean up.

--- go/printer/print.go
+++ go/printer/print.go
@@ -5,11 +5,11 @@ import "fmt"
 // Print puts the formatted line on stdout for the user's clicking pleasure
 func Print(line []string) {
 	url := fmt.Sprintf("https://xkcd.com/%s", line[0])
-	// transcript := line[1]
-	// alt := line[2]
+	transcript := line[1]
+	alt := line[2]
 	fmt.Println("URL:\t\t", url)
-	fmt.Println("Alt:\t\t", line[2])
-	fmt.Println("Transcript:\t", line[1])
+	fmt.Println("Alt:\t\t", alt)
+	fmt.Println("Transcript:\t", transcript)
 	fmt.Println("* * * * *")
 
 }
