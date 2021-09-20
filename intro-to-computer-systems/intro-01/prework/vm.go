49cb0d3c59df2a333ccb669b5ed3f2290e08af72
chrisbodhi <chris.boette@data.world>
2021-09-20 18:55:03 -0500

    Different approach.

--- intro-to-computer-systems/intro-01/prework/vm.go
+++ intro-to-computer-systems/intro-01/prework/vm.go
@@ -29,52 +29,68 @@ const (
 //
 func compute(memory []byte) {
 	// Program command (Load, Store, Add, etc), first arg, second arg
-	pc := byte(8)
-	registers := [3]byte{pc, 0, 0} // PC, R1 and R2
-
+	registers := [3]byte{8, 0, 0} // PC, R1 and R2
+	for i, v := range memory[0:30] {
+		if i == 0 {
+			fmt.Println("===Data===")
+		}
+		if i == 8 {
+			fmt.Println("===Instruction===")
+		}
+		fmt.Println(i, ": ", v)
+	}
 	// Keep looping, like a physical computer's clock
 	for {
-		registers[0] = memory[pc]
-		registers[1] = memory[pc+1]
-		registers[2] = memory[pc+2]
-
-		op := registers[0] // fetch the opcode
-		r1 := registers[1]
-		r2 := registers[2]
+		op := memory[registers[0]] // fetch the opcode
+		mem1 := memory[registers[0]+1]
+		mem2 := memory[registers[0]+2]
 
 		// decode and execute
 		switch op {
 		// load		r1 addr		# Load value at given address into given register
 		case Load:
-			memory[r1] = memory[r2]
+			fmt.Println("Load", registers)
+			registers[mem1] = memory[mem2]
+			// fmt.Println("After load", registers)
 		// store	r2 addr		# Store the value in register at the given memory address
 		case Store:
-			memory[r2] = memory[r1]
+			fmt.Println("Store", registers, memory[registers[0]:registers[0]+3])
+			memory[mem2] = registers[mem1]
+			// fmt.Println("After store", memory[0:8])
 		// add		r1 r2		# Set r1 = r1 + r2
 		case Add:
-			memory[r1] += memory[r2]
+			fmt.Println("Add", registers)
+			registers[mem1] += registers[mem2]
 		// sub		r1 r2		# Set r1 = r1 - r2
 		case Sub:
-			memory[r1] -= memory[r2]
+			fmt.Println("Sub", registers)
+			registers[mem1] -= registers[mem2]
 		// jump		r1			# Set pc to pc + r1
 		case Jump:
-			pc = r1
-			return
-		// beqz		r1 r2		# If memory at r1 equals 0, increment the pc by the amount at r2; otherwise, continue
+			fmt.Println("Jump", registers)
+			registers[0] = mem1
+			continue
+		// beqz		r1 r2		# If memory at r2 equals 0, increment the pc by the amount at r1; otherwise, continue
 		case Beqz:
-			if memory[r1] == 0 {
-				pc += memory[r2]
-				return
+			fmt.Println("Beqz", registers, memory[registers[0]+2])
+			if registers[1] == 0 {
+				registers[0] += mem2
 			}
+			fmt.Println("End of Beqz", registers)
+			continue
 		// addi		r1 r2		# Add values passed as args, not adding what's in store
 		case Addi:
-			// TODO
-			fmt.Println("Addi", registers)
+			registers[mem1] += mem2
+		// subi		r1 r2		# Subtract
+		case Subi:
+			fmt.Println("Subi", registers, memory[registers[0]:registers[0]+3])
+			registers[mem1] -= mem2
 		// no params
 		case Halt:
+			fmt.Println("~~~Halt~~~")
 			return
 		}
 		// Incrememnt program counter
-		pc += byte(len(registers))
+		registers[0] += byte(len(registers))
 	}
 }
