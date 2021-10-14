53bd13911007de0638dd7e7c3992ef3f161f7d09
chrisbodhi <chris.boette@data.world>
2021-10-13 22:51:25 -0500

    Add exit, cd commands.

--- intro-to-computer-systems/simple-shell/shell.go
+++ intro-to-computer-systems/simple-shell/shell.go
@@ -11,6 +11,10 @@ import (
 // S is So Srs, it'S uppercaSe.
 const S = "🐢 "
 
+var state = map[string]bool{
+	"continue": true,
+}
+
 func looper() ([]byte, error) {
 	var s []byte
 	reader := bufio.NewReader(os.Stdin)
@@ -18,7 +22,7 @@ func looper() ([]byte, error) {
 	for {
 		b, err := reader.ReadByte()
 		if err != nil {
-			fmt.Println("\nSee ya, slow poke. 💨")
+			leave()
 			return s, err
 		}
 		if b == '\n' {
@@ -29,11 +33,19 @@ func looper() ([]byte, error) {
 	return s, nil
 }
 
-func eval(b []byte) {
-	input := string(b)
-	inputs := strings.Fields(input)
-	userCmd := inputs[0]
-	userArgs := inputs[1:]
+// 'exit' command
+func leave() {
+	state["continue"] = false
+	fmt.Println("\nSee ya, slow poke. 💨")
+}
+
+// 'cd' command
+func changeDir(s string) {
+	os.Chdir(s)
+}
+
+// default; calls to parent shell
+func callOut(userCmd string, userArgs []string) {
 	cmd := exec.Command(userCmd, userArgs...)
 	output, err := cmd.Output()
 	if err != nil {
@@ -42,13 +54,40 @@ func eval(b []byte) {
 	fmt.Printf("%s", output)
 }
 
+func eval(b []byte) {
+	input := string(b)
+	inputs := strings.Fields(input)
+	if len(inputs) > 0 {
+		userCmd := inputs[0]
+		userArgs := inputs[1:]
+		switch userCmd {
+		case "exit":
+			leave()
+			return
+		case "cd":
+			var dest string
+			if len(userArgs) == 0 {
+				dest = "/"
+			} else {
+				dest = userArgs[0]
+			}
+			changeDir(dest)
+			return
+		default:
+			callOut(userCmd, userArgs)
+		}
+	}
+}
+
 func handleInput() {
 	bs, err := looper()
 	if err != nil {
 		return
 	}
 	eval(bs)
-	handleInput()
+	if state["continue"] {
+		handleInput()
+	}
 }
 
 func main() {
