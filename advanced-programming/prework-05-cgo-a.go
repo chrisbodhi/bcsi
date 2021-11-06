7535d97eb59d983f8bc148f03c807997b2b4d59c
Chris Boette <cboette@gmail.com>
2021-11-05 22:19:04 -0500

    Initial cgo prework.

---
+++ advanced-programming/prework-05-cgo-a.go
@@ -0,0 +1,13 @@
+package main
+
+// #include "square.c"
+import "C"
+
+import "fmt"
+
+func main() {
+	n := C.int(5)
+	sq := C.square(n)
+	fmt.Println(fmt.Sprintf("%d squared is %d, which is type %T",
+		n, sq, sq))
+}
