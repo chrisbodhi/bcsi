87fecc0dc2931c55c8b200e992ea1225cdd0cc43
Chris Boette <cboette@gmail.com>
2022-02-13 11:04:25 -0600

    Start standalone skiplist implementation.

---
+++ ds-for-storage-and-retrieval/02-memtable/skiplist.go
@@ -0,0 +1,127 @@
+package memtable
+
+import (
+	"bytes"
+	"math/rand"
+)
+
+const p = 0.25 // pg. 5
+const MaxLevel = 8
+
+// "Because these data structures are linked lists with extra pointers that skip over intermediate nodes, I named them _skip lists_."
+
+type List struct {
+	Header Node
+	Level  int
+}
+
+type Node struct {
+	Forward []Node // len(Forward) is this node's level: "A node that has k forward pointers is called a level k node."
+	Key     []byte
+	Value   []byte
+	// Level	int // the paper says we don't need to store the level of the node in the node (p. 1)
+}
+
+func (list *List) Search(searchKey []byte) ([]byte, bool) {
+	x := list.Header
+	for i := list.Level; i >= 0; i-- {
+		for bytes.Compare(x.Forward[i].Key, searchKey) == -1 {
+			x = x.Forward[i]
+		}
+	}
+
+	x = x.Forward[0]
+	if bytes.Equal(x.Key, searchKey) {
+		return x.Value, true
+	}
+
+	return []byte{}, false
+}
+
+func (list *List) Insert(searchKey, newValue []byte) {
+	var update [MaxLevel]Node
+
+	x := list.Header
+	for i := list.Level; i >= 0; i-- {
+		xKeyLessThan := bytes.Compare(x.Forward[i].Key, searchKey) == -1
+		for xKeyLessThan {
+			if len(x.Forward) > 0 {
+				x = x.Forward[i]
+			} else {
+				break
+			}
+		}
+		update[i] = x
+	}
+
+	x = x.Forward[0]
+	if bytes.Equal(x.Key, searchKey) {
+		x.Value = newValue
+	} else {
+		lvl := randomLevel()
+		if lvl > list.Level {
+			for i := list.Level + 1; i <= lvl; i++ {
+				update[i] = list.Header
+			}
+			list.Level = lvl
+		}
+		x = makeNode(lvl, searchKey, newValue) // newValue is written just as "value" in the paper -- this is a guess
+		for i := 0; i <= list.Level; i++ {     // list.Level is written just as "level" in the paper -- this is a guess
+			x.Forward[i] = update[i].Forward[i]
+			update[i].Forward[i] = x
+		}
+	}
+	// TODO how does this do its update in-place?
+}
+
+func (list *List) Delete(searchKey []byte) {
+	var update [MaxLevel]Node
+
+	x := list.Header
+	for i := list.Level; i >= 0; i-- {
+		for bytes.Compare(x.Forward[i].Key, searchKey) == -1 {
+			x = x.Forward[i]
+		}
+		update[i] = x
+	}
+	x = x.Forward[1]
+	if bytes.Equal(x.Key, searchKey) {
+		for i := 1; i <= list.Level; i++ {
+			if !areEqual(update[i].Forward[i], x) {
+				break
+			}
+			update[i].Forward[i] = x.Forward[i]
+		}
+		for list.Level > 0 && isEmpty(list.Header.Forward[list.Level]) {
+			list.Level -= 1
+		}
+	}
+}
+
+func randomLevel() int {
+	lvl := 0
+	for rand.Float32() < p && lvl < MaxLevel {
+		lvl += 1
+	}
+	return lvl
+}
+
+func makeNode(lvl int, searchKey, value []byte) Node {
+	return Node{Forward: []Node{}, Key: searchKey, Value: value}
+}
+
+func areEqual(a, b Node) bool {
+	return bytes.Equal(a.Key, b.Key) && bytes.Equal(a.Value, b.Value)
+}
+
+func isEmpty(n Node) bool {
+	return len(n.Forward) == 0 && len(n.Key) == 0 && len(n.Value) == 0
+}
+
+func BuildForwardList(levels int) []Node {
+	fwd := []Node{}
+	for i := 0; i < levels; i++ {
+		fwd = append(fwd, Node{})
+	}
+	return fwd
+}
---
+++ ds-for-storage-and-retrieval/02-memtable/skiplist_test.go
@@ -0,0 +1,41 @@
+package memtable
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestBuildForwardList(t *testing.T) {
+	levels := 6
+	list := BuildForwardList(levels)
+	if len(list) != levels {
+		t.Fatalf("Expected %d nodes in the forward list, but got %d.\n", levels, len(list))
+	}
+}
+
+func TestSearch(t *testing.T) {
+	// "The header of a list has forward pointers at levels one through MaxLevel."
+	headerForward := BuildForwardList(MaxLevel)
+	if len(headerForward) != MaxLevel {
+		t.Fatalf("Expected %d nodes in the forward list, but got %d.\n", MaxLevel, len(headerForward))
+	}
+	header := Node{headerForward, []byte("abc"), []byte("012")}
+	list := List{header, MaxLevel - 1}
+
+	insertVal0 := []byte("345")
+	list.Insert([]byte("def"), insertVal0)
+
+	insertVal1 := []byte("678")
+	list.Insert([]byte("ghi"), insertVal1)
+
+	insertVal2 := []byte("901")
+	list.Insert([]byte("jkl"), insertVal2)
+
+	val, ok := list.Search([]byte("def"))
+	if !ok {
+		t.Fatalf("Did not retrieve %s, but instead %s.", insertVal0, val)
+	}
+	if !bytes.Equal(insertVal0, val) {
+		t.Fatalf("wtf? %s", val)
+	}
+}
