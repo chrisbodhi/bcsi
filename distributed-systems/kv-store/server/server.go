35dc952f9d58eab82b3c7a9328fdb5f86ecafe4b
chrisbodhi <chris.boette@data.world>
2023-04-22 14:21:45 -0400

    1.3 Persist data to disk.
    Contains data.

--- distributed-systems/kv-store/server/server.go
+++ distributed-systems/kv-store/server/server.go
@@ -1,10 +1,46 @@
 package server
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
 
 var mem = make(map[string]string)
 
+var STORAGE = "storage.json"
+
+func init() {
+	loadDatastore()
+	fmt.Println(fmt.Sprintf("Successfully opened %s\nand loaded it into memory:", STORAGE), mem)
+}
+
+func loadDatastore() {
+	jsonFile, err := os.Open(STORAGE)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+	byteValue, _ := io.ReadAll(jsonFile)
+	json.Unmarshal(byteValue, &mem)
+}
+
+func updateDatastore() {
+	jsonFile, err := os.OpenFile(STORAGE, os.O_RDWR, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+	jsonData, err := json.Marshal(mem)
+	if err != nil {
+		fmt.Println(err)
+	}
+	jsonFile.Write(jsonData)
+}
+
 func Get(key string) {
+	loadDatastore()
 	// TODO: error handling
 	fmt.Println(mem[key])
 }
@@ -12,5 +48,7 @@ func Get(key string) {
 func Set(key, value string) {
 	// TODO: error handling
 	mem[key] = value
-	fmt.Println("set", key, value, "successfully")
+	// Flush mem to storage.json
+	updateDatastore()
+	fmt.Println(key, value)
 }
